ddafdc462e4b2d2941643c39a059090994b1d517
toriopmot <tom.poirot1@gmail.com>
2022-04-01 11:33:37 +0200

    V2 avec photos

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,69 @@
+    // async function getPhotographers() {
+    //     // Penser à remplacer par les données récupérées dans le json
+        
+    //      async function getPhotographers() {
+    //     // Penser à remplacer par les données récupérées dans le json
+    //     let jsonPhotographers = ('../../data/photographers.json')
+    //     fetch(jsonPhotographers)
+    //         .then(res => console.log(res))
+    //         .then(res2 => console.log(res2.photographers))
+    //         .catch(error => console.log(error));
+    //     console.log(jsonPhotographers)
+    //     // et bien retourner le tableau photographers seulement une fois
+    //     return ({
+    //         jsonPhotographers: [...jsonPhotographers]})
+    // }
+
+    // async function getPhotographers() {
+    //     fetch('../../data/photographers.json')
+    //         .then(res => res.json())   
+    //         .then(data => {
+    //             let allPhotographers = data.photographers
+    //             // console.log(a)
+    //             allPhotographers.forEach(e => {
+    //                 console.log(e)
+    //             });
+    //         })
+    //         .catch(error => console.log(error));
+    // }
+
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+    
+        await fetch("../../data/photographers.json")
+            .then((res) => res.json())
+            .then((data) => (photographers = data.photographers));
+    
         // et bien retourner le tableau photographers seulement une fois
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]
+        })
     }
+
+    //     const photographers = [
+    //         {
+    //             "name": "Ma data test",
+    //             "id": 1,
+    //             "city": "Paris",
+    //             "country": "France",
+    //             "tagline": "Ceci est ma data test",
+    //             "price": 400,
+    //             "portrait": "account.png"
+    //         },
+    //         {
+    //             "name": "Autre data test",
+    //             "id": 2,
+    //             "city": "Londres",
+    //             "country": "UK",
+    //             "tagline": "Ceci est ma data test 2",
+    //             "price": 500,
+    //             "portrait": "account.png"
+    //         },
+    //     ]
+    //     // et bien retourner le tableau photographers seulement une fois
+    //     return ({
+    //         photographers: [...photographers, ...photographers, ...photographers]})
+    // }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
