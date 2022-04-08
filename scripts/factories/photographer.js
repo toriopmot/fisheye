72c8fff9e835b2fb075113dc421591e4d629a524
toriopmot <tom.poirot1@gmail.com>
2022-04-08 16:03:25 +0200

    V3

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,34 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        /* */
+        const click = document.createElement ( 'a' );
+        const loc = document.createElement ('p');
+        const citation = document.createElement ('p');
+        const prix = document.createElement ('p');
+
+        click.href = "./photographer.html";
+        click.style.textDecoration = "none"
+        loc.textContent = city + ", " + country;
+        citation.textContent = tagline;
+        prix.textContent = price + "€/jour";
+        
+        
+        click.setAttribute("id", id)
+
+        /* */
+        article.appendChild(click)
+
+        click.appendChild(img);
+        click.appendChild(h2);
+
+        /* */
+        article.appendChild(loc);
+        article.appendChild(citation);
+        article.appendChild(prix);
+        /* */
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
